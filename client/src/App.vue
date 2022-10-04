137796486c9040855274792914b210b4f4c1ad82
Tony Ramírez <tonyramirez.business@outlook.com>
2022-10-03 21:50:57 -0500

    getting item component from api

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,14 +1,35 @@
 <template>
-  <div id="app"></div>
+  <div id="app">
+    <div class="notification" v-for="(item, i) in items" :key="item._id">
+      <p>
+        <span class="tag is-primary">{{ i + 1 }}</span>
+        {{ item.description }}
+      </p>
+    </div>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import axios from 'axios'
 
 export default {
   name: 'App',
+  data() {
+    return {
+      items: []
+    }
+  },
+  async mounted() {
+    const response = await axios.get('api/bucketListItems/')
+    this.items = response.data
+  }
 }
 </script>
 
 <style>
+#app {
+  margin: auto;
+  margin-top: 3rem;
+  max-width: 700px;
+}
 </style>
