43d9928a51fd92d048a8632df99c213338db4e33
Tony Ramírez <tonyramirez.business@outlook.com>
2022-10-03 19:54:21 -0500

    setup other modules

--- server.js
+++ server.js
@@ -1,10 +1,23 @@
+// dotenv: Load env variables
 if (process.env.NODE_ENV !== 'production') {
   require('dotenv').config();
 }
+// express: Load ExpressJS
 const express = require('express')
 const app = express()
+// mongoose: Load MongoDB
 const mongoose = require('mongoose')
 const { PORT, mongoUri } = require('./config')
+// cors: AJAX req from back to front
+const cors = require('cors')
+// morgan: Log every request to sv in the console
+const morgan = require('morgan')
+// bodyParser: transform body of every req to json
+const bodyParser = require('body-parser')
+
+app.use(cors())
+app.use(morgan('tiny'))
+app.use(bodyParser.json())
 
 mongoose
   .connect(mongoUri, {})
