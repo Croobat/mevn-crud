c9cb270df5d9c1f58e63df47f34318da8531c775
Tony Ramírez <tonyramirez.business@outlook.com>
2022-10-03 20:04:26 -0500

    added api get method

--- routes/api/bucketListItems.js
+++ routes/api/bucketListItems.js
@@ -2,3 +2,18 @@ const { Router } = require('express')
 const BucketListItem = require('../../models/BucketListItem')
 
 const router = Router()
+
+// API get method
+router.get('/', async (req, res) => {
+  try {
+    const bucketListItems = await BucketListItem.find()
+    if (!bucketListItems) throw new Error('No bucketListItems')
+    const sorted = bucketListItems.sort((a, b) => {
+      let dateDiff = new Date(a.date).getTime() - new Date(b.date).getTime()
+      return dateDiff
+    })
+    res.status(200).json(sorted)
+  } catch (error) {
+    res.status(500).json({ message: error.message })
+  }
+})
