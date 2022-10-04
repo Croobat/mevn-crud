7ab75b73a8e5fb46131d81187b650d3f9b758125
Tony Ramírez <tonyramirez.business@outlook.com>
2022-10-03 19:49:43 -0500

    setup MongoDB

--- server.js
+++ server.js
@@ -1,6 +1,15 @@
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config();
+}
 const express = require('express')
 const app = express()
-const PORT = 3000
+const mongoose = require('mongoose')
+const { PORT, mongoUri } = require('./config')
+
+mongoose
+  .connect(mongoUri, {})
+  .then(() => console.log('MongoDB db connected...'))
+  .catch((err) => console.log(err))
 
 app.get('/', (req, res) => res.send('Holaa'))
 
