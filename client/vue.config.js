acfaaa699ddfb6344dc81c8ffa75cb39daeeecdf
Tony Ramírez <tonyramirez.business@outlook.com>
2022-10-03 19:22:49 -0500

    setup client

---
+++ client/vue.config.js
@@ -0,0 +1,30 @@
+const { defineConfig } = require('@vue/cli-service')
+
+module.exports = defineConfig({
+  transpileDependencies: true,
+})
+
+// module.exports = {
+//   configureWebpack: {
+//     devServer: {
+//       proxy: {
+//         '/api': {
+//           target: 'http://localhost:3000',
+//         }
+//       }
+//     }
+//   }
+// }
+
+module.exports = {
+  devServer: {
+    proxy: {
+      '^/api': {
+        target: 'http://localhost:5000',
+        changeOrigin: true,
+        logLevel: 'debug',
+        pathRewrite: { '^/api': '' },
+      },
+    },
+  },
+}
